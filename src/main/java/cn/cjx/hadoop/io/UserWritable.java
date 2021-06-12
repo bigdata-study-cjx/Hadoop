167a8242e7140afb18afd5234b3cfdf4648dc84c
chenjinxin <chenjinxin@chenjinxin1124.cn>
2021-06-13 00:45:21 +0800

    自定义mr数据类型

---
+++ src/main/java/cn/cjx/hadoop/io/UserWritable.java
@@ -0,0 +1,76 @@
+package cn.cjx.hadoop.io;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Objects;
+
+public class UserWritable implements Writable {
+
+    private int id;
+    private String name;
+
+    public UserWritable(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void set(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(id);
+        out.writeUTF(name);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        this.id = in.readInt();
+        this.name = in.readUTF();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserWritable that = (UserWritable) o;
+
+        if (id != that.id) return false;
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserWritable{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
